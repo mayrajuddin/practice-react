a49aac2d587e1c901588330a3c2cae6bebeb060b
uddinmayraj <mayrajuddin03@gmail.com>
2022-09-22 19:50:56 +0600

    add

---
+++ src/components/users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// const TextStyle = {
+//     color: 'red',        //style={TextStyle}
+// }
+const Users = () => {
+    return (
+        <div>
+            <h1>hello world ! im here to reign the world ..</h1>
+        </div>
+    );
+};
+
+export default Users;
